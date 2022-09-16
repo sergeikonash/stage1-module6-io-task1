5c8da539cf5ecba9b2bff7e35aed4e5a1b89fb1a
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-09-16 11:19:19 +0200

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,6 +6,41 @@ import java.io.File;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        String fromFile = "";
+        String[] subStr = new String[4];
+        try (FileInputStream fileInputStream = new FileInputStream(file)){
+            int ch;
+            while((ch = fileInputStream.read()) != -1) {
+                fromFile += (char)ch;
+            }
+            String delimeter = "\n";
+            subStr = fromFile.split(delimeter);
+            String deleteName = "Name: ";
+            String deleteAge = "Age: ";
+            String deleteEmail = "Email: ";
+            String deletePhone = "Phone: ";
+            for (int i = 0; i < subStr.length; i++) {
+                subStr[i] = subStr[i].replace(deleteName, "");
+                subStr[i] = subStr[i].replace(deleteAge, "");
+                subStr[i] = subStr[i].replace(deleteEmail, "");
+                subStr[i] = subStr[i].replace(deletePhone, "");
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        profile.setName(subStr[0]);
+        try {
+            profile.setAge(Integer.parseInt(subStr[1].trim()));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        profile.setEmail(subStr[2]);
+        try {
+            profile.setPhone(Long.parseLong(subStr[3].trim()));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
